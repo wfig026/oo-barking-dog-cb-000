948326d33e6fc5676cae852ec728e25c14e896e8
Will Figueiredo <will@wfigueiredo.com>
2018-08-21 05:32:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,12 +14,12 @@ class Dog
   end
 end
 
-fido = Dog.new
-fido.name = "Fido"
-
-fido.name
-# => "Fido"
-
-fido.bark
-woof!
-# => nil
+# fido = Dog.new
+# fido.name = "Fido"
+# 
+# fido.name
+# # => "Fido"
+# 
+# fido.bark
+# woof!
+# # => nil
