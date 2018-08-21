5dc37a342a088715681d9e5507f7c3eaaa376577
Will Figueiredo <will@wfigueiredo.com>
2018-08-21 05:36:16 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Dog
+  def instantiate
+    @name
+  end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def bark
+    puts "woof!"
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+
+puts "#{fido.name}"
+# => "Fido"
+
+puts "#{fido.bark}"
+# woof!
+# => nil
