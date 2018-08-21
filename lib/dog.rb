0f7bd0af970ea9177a281ae5f7c5e793e9c3649c
Will Figueiredo <will@wfigueiredo.com>
2018-08-21 05:34:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,6 +10,16 @@ class Dog
     @name
   end
   def bark
-    puts "wleaoof!"
+    puts "woof!"
   end
 end
+
+fido = Dog.new
+fido.name = "Fido"
+
+fido.name
+# => "Fido"
+
+fido.bark
+woof!
+# => nil
